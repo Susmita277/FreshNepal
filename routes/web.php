bb406d55f9460edb0433a8c36e8266154361a7f2
Susmita277 <susmetaadangal@gmail.com>
2025-11-10 17:06:42 +0545

    required route added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Livewire\Home;
+use App\Livewire\Product;
+use App\Livewire\ProductDetail;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/',Home::class)->name('home');
+Route::get('/', Home::class)->name('home');
+Route::get('/products' ,Product::class)->name('product');
+Route::get('/productsdetails' ,ProductDetail::class)->name('product-details');

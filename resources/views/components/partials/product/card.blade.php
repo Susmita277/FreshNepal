3649f0f443fb4d375fa24c4907f56e6bca2123ac
Susmita277 <susmetaadangal@gmail.com>
2025-11-10 17:05:18 +0545

    component addded for product  card

---
+++ resources/views/components/partials/product/card.blade.php
@@ -0,0 +1,20 @@
+     <div class="bg-white rounded-3xl fit p-5 h-[230px] flex flex-col items-center relative">
+         <div class="h-[100px] ">
+             <img src="https://png.pngtree.com/png-vector/20240803/ourmid/pngtree-carrot-with-green-top-clipart-illustration---graphic-png-image_13366428.png"
+                 class="object-contain cl
+                    w-full h-full">
+         </div>
+         <div class=" mt-2 ">
+             <h3 class="text-xl font-medium font-poppins text-center">Fresh Carrot</h3>
+             <p class="font-inter tracking-tight line-spacing-1 font-medium text-gray-500 text-sm text-center">
+                 Root Vegetables / kg</p>
+         </div>
+         <div class="flex items-end justify-center relative">
+             <h4 class="mt-4 text-md  font-medium font-poppins text-highlight text-center ">NPR.50</h4>
+             <x-heroicon-o-plus-circle class="w-5 h-5 absolute top-0 -right-15 top-4 cursor-pointer" />
+         </div>
+         <div
+             class="w-fit h-fit rounded-full p-1 text-white bg-green-400 text-xs tracking-tight font-inter flex justify-center items-center absolute top-2 left-2">
+             20% off
+         </div>
+     </div>

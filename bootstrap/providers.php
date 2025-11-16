6e5b0a763ec5c6e1eb9d31cc9bce1aaaeb280c40
Susmita277 <susmetaadangal@gmail.com>
2025-11-16 11:03:27 +0545

    required setup

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,4 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\VendorPanelProvider::class,
-    App\Providers\FortifyServiceProvider::class,
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,32 @@
 <?php
 
+use App\Livewire\Checkout;
 use App\Livewire\Home;
+use App\Livewire\Cart;
+use App\Livewire\OrderDetails;
 use App\Livewire\Product;
 use App\Livewire\ProductDetail;
+use App\Livewire\Register;
+use App\Livewire\Login;
+use App\Livewire\VerifyVendor;
+use App\Livewire\VendorLogin;
+use App\Livewire\VendorRegister;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\RoleMiddleware;
+
 
 
 Route::get('/', Home::class)->name('home');
-Route::get('/products' ,Product::class)->name('product');
-Route::get('/productsdetails' ,ProductDetail::class)->name('product-details');
+Route::get('/products', Product::class)->name('products');
+Route::get('/products/{slug}', ProductDetail::class)->name('product-details');
+Route::get('/cart', Cart::class)->name('cart');
+Route::get('/checkout', Checkout::class)->name('checkout');
+Route::get('/order-details', OrderDetails::class)->name('order-details');
+Route::get('/user-register', Register::class)->name('user-register');
+Route::get('/user-login', Login::class)->name('user-login');
+Route::get('/vendor/verify', VerifyVendor::class)->name('seller-verify');
+Route::get('/vendor/register', VendorRegister::class)->name('seller-register');
+
+
+
+  

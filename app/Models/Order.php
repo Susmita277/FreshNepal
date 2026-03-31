d24a0a73f3a4db86259f0313e9e5202086ae75c5
Susmita277 <susmetaadangal@gmail.com>
2026-03-31 16:29:46 +0545

    protect vendor items

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,24 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    protected $with = ['items.vendor'];
     protected $fillable = [
-        'user_id', 
-        'total_amount', 
+        'user_id',
+        'total_amount',
         'subtotal',
-        'delivery_charge', 
-        'status', 
+        'delivery_charge',
+        'status',
         'delivery_address',
         'city',
-        'area', 
-        'payment_method', 
+        'area',
+        'payment_method',
         'order_date'
     ];
 
-    public function user() { 
-        return $this->belongsTo(User::class); 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
-    
-    public function items() { 
-        return $this->hasMany(OrderItem::class); 
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
     }
 }
